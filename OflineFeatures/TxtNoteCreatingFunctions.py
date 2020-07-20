de65f6d1b4a10bfd9e3ea71083bde8d43623d761
adityaaravi <adityaaravi6@gmail.com>
2020-07-19 18:52:58 -0700

    JUL 19, 2020 - 6:52 PM ==> Code to enable the edit more started. There are some bugs whose roots haven't been found yet.

--- OflineFeatures/TxtNoteCreatingFunctions.py
+++ OflineFeatures/TxtNoteCreatingFunctions.py
@@ -41,31 +41,55 @@ class EditorInstance:
             # Printing a legend of all commands available
             self.printAvailableCommands()
 
-            while True:
-                current_line = input(str(len(self.lines) - 1) + ": ")
+            # listening to all commands and running the editor
+            self.editorRunner()
 
-                # checking if any special commands are called and executing them as necessary
-                # and also quiting or skipping to the next iteration of the while loop as necessary.
-                quit_auth, continue_auth = self.editorCommands(current_line)
-                if(quit_auth): break
-                if(continue_auth): continue
-
-                #Adding the current line to a List for later use once it is confirmed to not be a command.
-                self.lines.append(current_line)
-
-            # add each line in the list "lines" to a txt file
-            print("Here is a preview of your file:/n----------------------------------------")
-            for line in self.lines:
-                print(line)
-            print("----------------------------------------")
-        else: pass
 
 # ####### Method to initiate the editing an existing file
     def editFile(self):
-        pass
+        try:
+            self.file_name = input("What is the name of the file you want to open: ")
+            file_handler = open(self.file_name, "r")
+            self.lines = file_handler.readlines()
+            file_handler.close()
+
+            # This part of the code runs if there is in fact a file with the name the user gave
+            print("File opened, here is a preview of what there is so far:\n----------------------------------------")
+            for line in self.lines:
+                print(str(self.lines.index(line)) + ": "+ line, end="")
+            print("----------------------------------------")
+
+            # Printing a list of all available commands
+            self.printAvailableCommands()
+            # Starting the editor runner.
+            self.editorRunner()
+
+        except():
+            create_new_file = input("No such file exists, Do you want to create a new one? (reply \"yes\" or \"no\"): ")
+            if(create_new_file.lower().startswith("yes")):
+                self.newFile()
+            else:
+                quit_auth = input("Do you want to retry typing the name of the file correctly or quit the text editor?"
+                                  + "(reply \"retry\" or \"quit\"): ").lower()
+                if quit_auth.startswith("retry"): self.editFile()
+
 
 # ######################################################## Commands common for both editing an existing file
     # #################################################### and creating a new file.
+
+    def editorRunner(self):
+        while True:
+            current_line = input(str(len(self.lines) - 1) + ": ")
+
+            # checking if any special commands are called and executing them as necessary
+            # and also quiting or skipping to the next iteration of the while loop as necessary.
+            quit_auth, continue_auth = self.editorCommands(current_line)
+            if (quit_auth): break
+            if (continue_auth): continue
+
+            # Adding the current line to a List for later use once it is confirmed to not be a command.
+            self.lines.append(current_line)
+
 
     # ### special commands to control the text editor. Returns true if we need to leave the text editor.
     def editorCommands(self, current_line):
@@ -80,6 +104,7 @@ class EditorInstance:
                 return True, False
             else:
                 print("operation cancelled, continue typing")
+                return False, True
             print("----------------------------------------")
         # command to edit a particular line of the file
         if current_line == "^^^^":
@@ -102,7 +127,7 @@ class EditorInstance:
             confirmation = input("Are you sure that you want to quit without saving?\n" +
                                  "Type \"y\" to confirm or any other key to cancel: ")
             if (confirmation.lower() == "y"):
-                lines = list()
+                self.lines = list()
                 return True, False
             else:
                 print("operation cancelled, continue typing")
@@ -125,15 +150,21 @@ class EditorInstance:
         print("----------------------------------------")
 
     def saveFile(self):
-        if(self.task == NEW_FILE_TOKEN):
-            file_handler = open(self.file_name, "w")
-            summative_string = ""
-            for line in self.lines:
-                summative_string += line + "\n"
+        print("Here is a preview of your file:/n----------------------------------------")
+        for line in self.lines:
+            print(line)
+        print("----------------------------------------")
+
+        # save file tasks that have to be done when creating a new file.
+        file_handler = open(self.file_name, "w")
+        summative_string = ""
+        for line in self.lines:
+            summative_string += line + "\n"
             file_handler.write(summative_string)
-            file_handler.close()
-            print("----------------------------------------")
-            print("Your file has been saved.")
-            print("----------------------------------------")
+
+        file_handler.close()
+        print("----------------------------------------")
+        print("Your file has been saved.")
+        print("----------------------------------------")
 
 
